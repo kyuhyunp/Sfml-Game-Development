5d96edcafb03cbcbd0ad2df6b8e5fa7cd05fee66
Kyuhyun Park <kyuhyunp@umich.edu>
2025-05-01 12:46:45 -0700

    Used template to create a ResourceHolder that can hold heavy weight resources

--- Sfml-Game-Development/Main.cpp
+++ Sfml-Game-Development/Main.cpp
@@ -1,7 +1,28 @@
-#include "Header/Game.hpp"
+#include "Header/ResourceHolder.hpp"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	Game game;
-    game.run();
+	ResourceHolder <sf::Texture, Textures::ID> textures;
+	textures.load(Textures::ID::Airplane, "Media/Textures/Eagle.png");
+
+	sf::Sprite playerPlane(textures.get(Textures::ID::Airplane));
+
+	sf::RenderWindow window(sf::VideoMode({ 640, 480 }), "SFML Application", sf::Style::Close);
+	
+	ResourceHolder <sf::Font, Fonts::ID> fonts;
+	fonts.open(Fonts::ID::Sansation, "Media/Sansation.ttf");
+	sf::Text text(fonts.get(Fonts::ID::Sansation), "Hello");
+	text.setPosition({ 200.f, 200.f });
+
+	while (true) {
+		window.clear();
+		window.draw(text);
+
+		window.draw(playerPlane);
+		window.display();
+	}
+
+	
+
 }
